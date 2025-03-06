3c791a5a3b08e4775f68220e7ea7fdc9b27547a4
unknown <Minskoleopc>
2025-03-06 22:19:51 +0530

    added

---
+++ script13.js
@@ -0,0 +1,30 @@
+let amol = {
+    firstName:"amol",
+    lastName:"rao",
+    age:12
+}
+
+let chinmay  ={
+    firstName:"chinmay",
+    lastName:"deshpande",
+    age:30
+
+}
+
+
+// program 2
+
+
+class  Person{
+
+    firstName = undefined
+    lastName = undefined
+    age  = undefined
+
+}
+let amolA = new Person()
+console.log(amolA)
+amolA.firstName = "amol"
+amolA.lastName = "rao"
+amolA.age = 32
+console.log(amolA)
