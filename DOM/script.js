d0fe363693efe4ac3504f4bd0968d34ff22a185e
gitminskole <git.minskole@gmail.com>
2025-03-20 22:23:13 +0530

    added

---
+++ DOM/script.js
@@ -0,0 +1,8 @@
+
+document.querySelector('h1')
+.addEventListener('click',function(){
+    document.querySelector('h1').textContent = "chinmay"
+    document.querySelector('h1').style.color = "green"
+    document.querySelector('h1').style.background = "red"
+
+})
