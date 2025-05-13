6d5d3a56a0f445dabc371cfb5d2bca339f3abd83
gitminskole <git.minskole@gmail.com>
2025-05-13 18:26:59 +0530

    added

---
+++ 5pm/DOM2/script.js
@@ -0,0 +1,21 @@
+
+//<h2 id = "one" class ="two" name="nm">Head</h2>
+// CSS - selector
+
+// id
+let byId = document.querySelector('#one')
+console.log(byId)
+// byClass
+let byClass = document.querySelector(".two")
+console.log(byClass)
+// by TagName
+let byTagName = document.querySelector('h2')
+console.log(byTagName)
+// byName - tagName[attribute="value"]
+let byName = document.querySelector("h2[name='nm']")
+console.log(byName)
+
+byId.addEventListener('click',function(){
+    byId.style.color = "red"
+    byId.textContent = "chinmay"
+})
