8613c56f04dafb4a1a5e2c82e84d0d4c02918ddb
gitminskole <git.minskole@gmail.com>
2025-07-18 07:54:36 +0530

    added

--- 630/day10.js
+++ 630/day10.js
@@ -57,3 +57,57 @@ console.log(city5.slice(-1,-4))
 
 // console.log(city5.length)
 // slice(startIndex, endIndex(not included))
+
+// includes
+let news = "northSouthEastWest"
+let q33 = news.includes("north")
+let q44 = news.includes("e")
+console.log(q33)
+console.log(q44)
+
+// first occurence
+let info = "i am learning javascript"
+let e2 = info.replace("javascript","python")
+console.log(e2)
+
+// all occurence
+let info2 = "i am learning python and python is easy"
+info2 = info2.replaceAll("python","javascript")
+console.log(info2)
+
+// indexof
+let city7 = "pune"
+// 0   1   2    3
+// p   u   n    e
+
+let e22 = city7.indexOf("n")
+e33 = city7.indexOf("N")
+console.log(e22)
+console.log(e33)
+
+let e44 = city7.at(1)
+console.log(e44)
+
+
+let city8 = "nagpur"
+
+let info3 = "chinmaydeshpande010@gmail.com"
+//["chinmaydeshpande010","gmail.com"]
+let e66  = info3.split("a")
+console.log(e66)
+
+info4 = "apple mango banana orange papaya"
+let e77 = info4.split(" ")
+console.log(e77)
+
+
+let city9 = "nagpur"
+city9 = city9.repeat(3)
+console.log(e88)
+console.log(city9)
+
+
+let n = [11,22,33,44,55]
+let ncopy = n.reverse()
+console.log(ncopy)
+console.log(n)
