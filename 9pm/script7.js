063c6e5b198425949caeafd070eab6810b921269
gitminskole <git.minskole@gmail.com>
2025-07-28 21:24:38 +0530

    added

---
+++ 9pm/script7.js
@@ -0,0 +1,91 @@
+
+// intialization
+// while(condition){
+    // statement 
+    // increment/ decrement
+//}
+
+// program 1
+
+let i1 = 1
+while(i1 <=3){
+    console.log(i1) // 1 // 2 // 3
+    i1 = i1 + 1 // 2 // 3 // 4
+}
+
+// program 2
+let i2 = 1
+while(i2 <= 3){
+    console.log("hello")
+    i2 = i2 + 1
+}
+
+// program 3
+// print 1 to 5
+
+let i3 =1
+while(i3 <= 5){
+    console.log(i3)// 1 // 2 // 3 // 4// 5
+    i3 = i3 + 1 // 2// 3 // 4 // 5 // 6
+}
+
+// program 4
+let i4 = 5
+while(i4 >= 1){
+    console.log(i4) // 5 // 4 // 3 // 2 //1
+    i4 = i4 - 1 // 4 // 3 // 2 // 1 // 0
+}
+
+// program 5 
+let i5 = 1
+while(i5 <= 10){
+    console.log(i5 * 2)
+    i5 = i5 + 1
+}
+
+// program 6
+
+let i6 = 2
+while(i6 <= 20){
+    console.log(i6)
+    i6 = i6 + 2
+}
+
+
+// program 7
+let i7 = 50
+while(i7 >= 5){
+    console.log(i7) // 50 // 45 ----- 5
+    i7  = i7 - 5 // 45 // 40 ----- 0 
+}
+
+
+//  break 
+
+//for loop with break statement
+
+let i8  = 1
+while(i8 <=5){
+    if(i8 == 3){
+        break
+    }
+    console.log(i8) // 1 // 2
+    i8 = i8 + 1 // 2 // 3
+}
+
+
+let i9  = 1
+while(i9 <=5){
+     console.log(i9)  // 1 //2 // 3
+    if(i9 == 3){
+        break
+    }
+   
+    i9 = i9 + 1  // 2 // 3
+}
+
+
+
+
+
+ 
