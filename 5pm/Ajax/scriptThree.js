7a80bb9f90b9c3f6a92b76b99c40b1873c49ab5c
thetanish1 <tanishdewase25@gmail.com>
2025-08-01 23:34:33 +0530

    practice on promises ajax

--- 5pm/Ajax/scriptThree.js
+++ 5pm/Ajax/scriptThree.js
@@ -19,7 +19,6 @@
 // additionA()
 // additionB()
 
-
 // function getInfo(){
 //     setTimeout(function(){
 //         console.log("user created ...")
@@ -48,7 +47,7 @@
 // }
 // getInfo()
 
-// // Promise --- async sync or parallel 
+// // Promise --- async sync or parallel
 
 // let pro = new Promise(function (resolve, reject) {
 //     let x = 10
@@ -69,7 +68,6 @@
 //         console.log(str)
 //     })
 
-
 // let pro2 = new Promise(function (resolve, reject) {
 //     let x = 10
 //     let y = 5
@@ -89,8 +87,6 @@
 // .catch(function(str){
 //     console.log(str)
 // })
-
-
 
 // let pro3 = new Promise(function (resolve, reject) {
 //     let x = 10
@@ -115,63 +111,59 @@
 //     console.log("i will always execute....")
 // })
 
- 
+// let pro4 = new Promise(function(resolve,reject){
+//     let a = 10
+//     let b = 10
+//     if(a == b){
+//         resolve("hello..")
+//     }
+//     else {
+//         reject("bye ....")
+//     }
 
-let pro4 = new Promise(function(resolve,reject){
-    let a = 10
-    let b = 10
-    if(a == b){
-        resolve("hello..")
-    }
-    else {
-        reject("bye ....")
-    }
+// })
 
-})
+// pro4
+// .then(function(str){
+//     console.log(str)
+//     return "hello ....2"
+// })
+// .then(function(str){
+//     console.log(str)
 
-pro4
-.then(function(str){
-    console.log(str)
-    return "hello ....2"
-})
-.then(function(str){
-    console.log(str)
+// })
+// .catch(function(str){
+//     console.log(str)
+// })
+// .finally(function(){
+//     console.log("i will always execute")
+// })
 
-})
-.catch(function(str){
-    console.log(str)
-})
-.finally(function(){
-    console.log("i will always execute")
-})
+// function userCreate (){
+//     return new Promise(function(resolve,reject){
+//         setTimeout(function(){
+//             resolve('user created')
+//         },3000)
+//     })
 
+// }
+// function Id (){
+//     return new Promise(function(resolve,reject){
+//         setTimeout(function(){
+//             resolve('user id')
+//         },2000)
+//     })
 
-function userCreate (){
-    return new Promise(function(resolve,reject){
-        setTimeout(function(){
-            resolve('user created')
-        },3000)
-    })
+// }
 
-}
-function Id (){
-    return new Promise(function(resolve,reject){
-        setTimeout(function(){
-            resolve('user id')
-        },2000)
-    })
+// function Info (){
+//     return new Promise(function(resolve,reject){
+//         setTimeout(function(){
+//             resolve('info')
+//         },1000)
+//     })
 
-}
-
-function Info (){
-    return new Promise(function(resolve,reject){
-        setTimeout(function(){
-            resolve('info')
-        },1000)
-    })
-
-}
-
+// }
 
 // userCreate()
 // .then(function(str){
@@ -186,7 +178,7 @@ function Info (){
 //     console.log(str)
 // })
 
-// asyn await 
+// asyn await
 
 // async function getUserInfo(){
 //     let r1 = await userCreate()
@@ -200,17 +192,126 @@ function Info (){
 //     console.log('End')
 // })
 
+// Practice................................................................
 
+// function add() {
+//   setTimeout(function () {
+//     console.log("Hello world");
+//   }, 2000);
+// }
 
+// function name() {
+//   setTimeout(function () {
+//     console.log("Hello from Tanish Dewase");
+//   }, 1000);
+// }
+// add();
+// name();
 
+// callback hell
 
+// function tanish() {
+//   setTimeout(function () {
+//     console.log("user created");
+//     setTimeout(function () {
+//       console.log("user id");
+//       setTimeout(function () {
+//         console.log("user info");
+//       }, 1000);
+//     }, 2000);
+//   }, 3000);
+// }
+// tanish();
 
+// callback function
 
+// function getcheese(callback) {
+//   setTimeout(() => {
+//     const cheese = "😊";
+//     console.log("Here is cheese" + cheese);
+//     callback(cheese);
+//   }, 2000);
+// }
+// getcheese((cheese) => {
+//     console.log("got the cheese"+cheese)
+// });
 
+// function userinput(callbackfunction){
+//     setTimeout(function(){
+//         const cheese="😊"
+//         console.log("here is the cheese"+cheese)
+//         callbackfunction(cheese)
+//     },3000);
+// }
 
+// userinput((cheese)=>{
+//     console.log("Got the cheese"+cheese)
+// })
 
+// function name(firstname,lastnamefun){
+//     console.log("Hello "+firstname);
+//     lastnamefun();
+// }
 
+// function lastnamefun(){
+//     console.log("Got it");
+// }
+// name("Tanish",lastnamefun)
 
+// call back hell demonstration
+
+// function getcheese(callback) {
+//   setTimeout(() => {
+//     const cheese = "😊";
+//     console.log("here i got cheese"+cheese);
+//     callback(cheese);
+//   }, 2000);
+// }
+
+// function makeDough(cheese,callback) {
+//   setTimeout(() => {
+//     const dough = "💕"+cheese;
+//     console.log("Here is Dough"+dough);
+//     callback(dough);
+//   }, 2000);
+// }
+
+// function makepizza(dough,callback) {
+//   setTimeout(() => {
+//     const pizza = "😍"+dough;
+//     console.log("Pizza is here"+pizza);
+//     callback(pizza);
+//   }, 1000);
+// }
+
+// getcheese((cheese) => {
+//   makeDough(cheese,(dough) => {
+//     makepizza(dough,(pizza) => {
+//       console.log("Ready with my pizza delivery"+pizza);
+//     });
+//   });
+// });
+
+// promises
+// resolve, reject, pending
+
+// let pro = new Promise(function (resolve, reject) {
+//   let a = 10;
+//   let b = 20;
+//   if (a != b) {
+//     resolve("Same value");
+//   } else {
+//     reject("Different Value");
+//   }
+// });
+// pro.then(
+//   function (message) {
+//     console.log(message);
+//   },
+//   function (error) {
+//     console.log(error);
+//   }
+// );
 
 
 
