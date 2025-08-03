df1e22a633ee999fddf08f4b507d185b6dc30d9e
thetanish1 <tanishdewase25@gmail.com>
2025-08-03 15:01:17 +0530

    Practiced the API fetching code

---
+++ 5pm/Ajax/tanish.js
@@ -0,0 +1,30 @@
+let clickButton=document.querySelector('#btn1')
+
+function userInfo(page){
+
+    fetch(`https://reqres.in/api/users?page=${page}`,{
+        method:'GET',
+        Headers:{
+            'Content-Type':'application/json',
+            'x-api-key':'reqres-free-v1'
+        }
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response){
+        response.data.forEach(function(el){
+            document.writeln(`<h1>${el.first_name}</h1>`)
+            document.writeln(`<h1> ${el.last_name}</h1>`)
+            document.writeln(`<img src=${el.avatar}>`)
+            document.writeln(`<h1>${el.id} ${el.email}</h1>`)
+        })
+    })
+    .catch(function(error){
+        console.log(error)
+    })
+}
+
+clickButton.addEventListener('click',function(){
+    userInfo(2)
+})
