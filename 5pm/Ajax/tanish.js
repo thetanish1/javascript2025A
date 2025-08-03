f5a833ba852b3ae22772cc3d37abb988d2f27d94
thetanish1 <tanishdewase25@gmail.com>
2025-08-03 17:28:38 +0530

    added bootstrap

--- 5pm/Ajax/tanish.js
+++ 5pm/Ajax/tanish.js
@@ -1,30 +1,28 @@
-let clickButton=document.querySelector('#btn1')
+let clickButton = document.querySelector("#btn1");
 
-function userInfo(page){
-
-    fetch(`https://reqres.in/api/users?page=${page}`,{
-        method:'GET',
-        Headers:{
-            'Content-Type':'application/json',
-            'x-api-key':'reqres-free-v1'
-        }
+function userInfo(page) {
+  fetch(`https://reqres.in/api/users?page=${page}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": "reqres-free-v1",
+    },
+  })
+    .then(function (response) {
+      return response.json();
     })
-    .then(function(response){
-        return response.json();
+    .then(function (response) {
+      response.data.forEach(function (el) {
+        document.writeln(`<h1>${el.first_name}</h1>`);
+        document.writeln(`<h1>${el.last_name}</h1>`);
+        document.writeln(`<h1>${el.email} ${el.id}</h1>`);
+        document.writeln(`<img src=${el.avatar}>`);
+      });
     })
-    .then(function(response){
-        response.data.forEach(function(el){
-            document.writeln(`<h1>${el.first_name}</h1>`)
-            document.writeln(`<h1> ${el.last_name}</h1>`)
-            document.writeln(`<img src=${el.avatar}>`)
-            document.writeln(`<h1>${el.id} ${el.email}</h1>`)
-        })
-    })
-    .catch(function(error){
-        console.log(error)
-    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
-
-clickButton.addEventListener('click',function(){
-    userInfo(2)
-})
+clickButton.addEventListener("click", function (page) {
+  userInfo(1);
+});
