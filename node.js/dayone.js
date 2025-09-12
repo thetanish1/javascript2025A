7c3ba47b0ac22d30d20c05458940c0d9d7f6f48e
thetanish1 <tanishdewase25@gmail.com>
2025-09-12 07:50:02 +0530

    added express learning folder

---
+++ node.js/dayone.js
@@ -0,0 +1,12 @@
+let names="Node.js"
+const version = 90;
+
+function greetname(){
+    return `hello ${names}`
+}
+console.log(greetname())
+
+let a = (a,b) => a + b;
+console.log(a(10,20 ))
+
+let namess=['tanish','suresh','ramesh']
