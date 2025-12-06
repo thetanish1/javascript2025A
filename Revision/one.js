15bd35e2dc03a6b92f422745a820ea30f7fff5f5
thetanish1 <tanishdewase25@gmail.com>
2025-12-06 22:07:04 +0530

    done

---
+++ Revision/one.js
@@ -0,0 +1,60 @@
+// let num = -7
+
+// if (num >0 && num < 10){
+//     console.log(" Positive number only")
+// }
+// else{
+//     console.log(" Negative number")
+// }
+
+// let r=10
+// let p=20
+
+// r>p? console.log(" R is greater "): console.log(" P is greater")
+
+// let t=0
+// t<=90?console.log("Positive number"):console.log('Negative number')
+
+// let i=1
+// while(i<=3){
+//     if (i==3){
+//         i=i+1
+//         continue
+//     }
+//     console.log(i)
+//     i=i+1
+// }
+
+// let t=1
+// while(t<=5){
+//     if(t==5){
+//         t=t+1
+//         continue
+//     }
+//     console.log(t)
+//     t=t+1
+// }
+
+// Switch case
+
+// let city="pune"
+// switch(city){
+//     case "pune":
+//         console.log(" MH")
+//         break
+
+//     case "Jaipur":
+//         console.log("Rajasthan")
+//         break
+
+//     case "Amristsar":
+//         console.log("Punjab")
+//         break
+
+//     default:
+//         console.log(" Invalid input")
+// }
+
+// Switch case with multipple swich case
+
+
